a7c6ebf0a93c8c385503c4aab11c3553b290788a
cagurzhan <1655929802@qq.com>
2023-02-19 22:20:30 +0800

    KMP

--- freshman_final/zixulie.cpp
+++ freshman_final/zixulie.cpp
@@ -1,28 +1,44 @@
 /*
 title: 期末复习
 from: C语言小测
-Tag: 双指针
+Tag: 双指针 KMP
 输入两个字符串str1和str2，判断字符串str1包含字符串str2的个数，允许重叠。
 */
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
-string str1;
-string str2;
+const int N = 1000010;
+string s,p;
+int ne[N];
+int cnt;
 int main(){
-    int res = 0;
-    int i=0;
-    int j=0;
-    cin >> str1 >> str2;
-    while(i < str1.size()){
-
-        while(str1[i]!=str2[j]) i++,j=0;
-
-        while(str1[i] == str2[j]) j++,i++;
-        if(j ==str2.size()) res++,i-=(str2.size()-2),j=0;
-        else j=0;
+    // getline(cin,s); 有时候需要读空格， 注意getline会读取空格
+    // getline(cin,p); 
+    cin >> s >> p;
+    int n= p.size();
+    int m = s.size();
+    //有点问题，需要在首部插入一个数，保证KMP从1开始，用insert！
+    s.insert(0,1,'0');
+    p.insert(0,1,'0');
+    //next数组
+    for(int i=2,j=0;i<=n;i++){
+        while(j && p[i]!=p[j+1]) j =ne[j];
+        if(p[i] == p[j+1] ) j++;
+        ne[i] = j;
     }
 
-    cout << res << endl;
+    //KMP匹配
+    for(int i=1,j=0;i<=m;i++){
+        while(j && s[i]!= p[j+1]) j= ne[j];
+        if(s[i] == p[j+1]) j++;
+        if(j==n){
+            cnt++;
+            j  = ne[j];
+        }
+    } 
 
+    cout << cnt << endl;
     return 0;
 }
---
+++ lanqiao/kmp.cpp
@@ -0,0 +1,40 @@
+/*
+title: 蓝桥杯复习
+from: 831 字符串匹配
+Tag: KMP
+Code: KMP匹配
+核心思路：匹配不成功就退到ne[j]
+*/
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+const int N = 10010, M = 100010;
+int n,m;
+char s[M],p[N]; // 模式串和匹配串
+int ne[N]; //ne数组，ne[i]=j 表示长度为j的，以i结尾的后缀 和以从1开始的前缀相
+int main(){
+    //下标从0开始
+    cin >> n >> p+1 >> m >> s+1 ;
+    
+    //求next数组
+    for(int i=2,j=0;i<=n;i++){ // 从2开始，ne[1]必定为0
+        while(j && p[i]!=p[j+1]) j = ne[j]; // 如果匹配不成功，则退到nej的位置
+        if(p[i] == p[j+1]) j++; //匹配成功
+        ne[i] = j; // 记录ne
+    }
+
+    //KMP匹配
+
+    for(int i=1,j=0;i<=m;i++){
+        while(j && s[i]!=p[j+1]) j = ne[j];
+        if(s[i] == p[j+1]) j++;
+        if(j == n){
+            //匹配成功
+            printf("%d",i-n+1 - 1); //下标从0开始
+            j = ne[j]; // 匹配成功后，
+        }
+    }
+    return 0;
+}
