13e765151704528d4b40dd95685083e8d1f3b26c
AjaxZhan <1655929802@qq.com>
2023-04-02 20:37:49 +0800

    区间dp

---
+++ level_improve/DP/区间DP/01.cpp
@@ -0,0 +1,36 @@
+/*
+title:
+from:
+Tag:
+Code:
+*/
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+const int N = 310;
+int n;
+int f[N][N];
+int s[N];
+int main(){
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cin >> s[i];
+        s[i]+=s[i-1];
+    }
+    //枚举区间
+    for(int len =2; len <=n;len++){
+        for(int i=1;i + len -1 <=n;i++){
+            int j = i + len -1;
+            f[i][j] = 1e8;
+            for(int k=i;k<j;k++){
+                //枚举分界点
+                f[i][j] = min(f[i][j],f[i][k]+f[k+1][j] + s[j]-s[i-1]);
+            }
+        }
+    }
+
+    printf("%d\n",f[1][n]);
+    return 0;
+}
---
+++ level_improve/DP/区间DP/02.cpp
@@ -0,0 +1,15 @@
+/*
+title: 能量项链
+from: acwing 320
+Tag: 区间dp
+Code:
+*/
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int main(){
+    
+    return 0;
+}
