5b8b709401e5739e7b2ebc7f21e89adbfdd13a36
CagurZhan <1655929802@qq.com>
2024-01-29 13:34:55 +0800

    doc:排序算法详细注释

--- Leetcode/Sort/cpp/各类排序算法.cpp
+++ Leetcode/Sort/cpp/各类排序算法.cpp
@@ -8,6 +8,7 @@ Description: 各类排序算法
 using namespace std;
 void quickSort(vector<int>&nums, int low , int high);
 int partition(vector<int>&nums,int low , int high);
+void quickSort(vector<int>&nums);
 
 // 针对Leetcode的题目，使用快速排序解决。
 vector<int> sortArray(vector<int>& nums) {
@@ -43,6 +44,8 @@ void bubbleSort(vector<int>&nums){
 }
 
 // 快速排序
+// 快排有点类似于说“前序递归”，就是先找枢纽进行划分，然后再递归操作。
+// 而归并排序有点类似于说“后序递归”，就是先递归划分，然后再进行归并操作。
 void quickSort(vector<int>&nums){
     // 打乱优化
     random_shuffle(nums.begin(),nums.end());
@@ -63,6 +66,9 @@ void quickSort(vector<int>&nums, int low , int high){
 }
 // 划分，返回划分后的中间位置，
 int partition(vector<int>&nums,int low , int high){
+    // 随机选择枢纽
+    int random_index = low + rand() % (high-low+1);
+    swap(nums[random_index],nums[low]);
     int pivot = nums[low];
     while(low < high){
         while(low < high && nums[high] >= pivot) high--;
@@ -77,19 +83,74 @@ int partition(vector<int>&nums,int low , int high){
 
 
 // 选择类排序：简单选择排序、堆排序
-// 简单选择排序
+// 简单选择排序，每次从序列后面选出最小值放到开头
 void selectionSort(vector<int>&nums){
-
+    for(int i=0;i<nums.size();i++){
+        int m = i;
+        for(int j=i+1;j<nums.size();j++){
+            if(nums[j] < nums[m]) m = j;
+        }
+        if(m!=i) swap(nums[i],nums[m]);
+    }
 }
-// 堆排序
-void heapSort(vector<int>&nums){
 
+
+// 堆排序
+// 调整操作，对元素K为根的子树进行调整
+void HeadAdjust(vector<int>&nums,int k,int len){
+    nums[0] = nums[k];
+    // k指向最终位置
+    for(int i=2*k;i<=len;i*=2){
+        if(i < len && nums[i] < nums[i+1]) i++;
+        if(nums[0] >= nums[i]) break;
+        else{
+            nums[k] = nums[i];
+            k = i;
+        }
+        nums[k] = nums[0];
+    }
+}
+// 建立大根堆
+// 堆的数组一般是下标从1开始
+void buildMaxHeap(vector<int>&nums, int len){
+    for(int i=len/2;i>0;i--)
+        HeadAdjust(nums,i,len);
+}
+
+void heapSort(vector<int>&nums){
+    buildMaxHeap(nums,nums.size()-1);
+    for(int i=nums.size()-1;i>1;i--){
+        swap(nums[i],nums[1]);
+        HeadAdjust(nums,1,i-1);
+    }
 }
 
 
 // 归并排序
-void mergeSort(vector<int>&nums){
 
+void merge(vector<int>&nums, int left, int mid , int right){
+    // 将元素复制到临时数组中
+    int i,j,k;
+    vector<int>tmp(nums);
+    for(i=left,j = mid+1,k = i;i<=mid && j<=right;k++){
+        if(tmp[i] <= tmp[j]) nums[k] = tmp[i++];
+        else nums[k] = tmp[j++]; 
+    }
+    while(i <= mid) nums[k++] = tmp[i++];
+    while(j <= right) nums[k++] = tmp[j++];
+}
+
+void mergeSort(vector<int>&nums, int left , int right){
+    if(left < right) {
+        int mid = (left + right) /2 ;
+        mergeSort(nums,left,mid);
+        mergeSort(nums,mid+1,right);
+        merge(nums,left,mid,right);
+    }
+}
+
+void mergeSort(vector<int>&nums){
+    mergeSort(nums,0,nums.size()-1);
 }
 
 // 此外还有基数排序，一般很少要求直接写出代码，因此这里不给出基数排序的代码。
@@ -106,12 +167,19 @@ void printArray(vector<int>&nums){
 int main(){
     
     vector<int> nums = {1,4,3,3,2,2,3,5,3,1,1,2,8,1,1,8,5,6,9};
+    // 用于测试堆排序，下标从1开始
+    vector<int> nums_for_heap = {999,1,4,3,3,2,2,3,5,3,1,1,2,8,1,1,8,5,6,9};
     cout << "before sort: " << endl;
+    // printArray(nums_for_heap);
     printArray(nums);
     // bubbleSort(nums);
     // insertSort(nums);
-    quickSort(nums);
+    // quickSort(nums);
+    // selectionSort(nums);
+    // heapSort(nums_for_heap);
+    mergeSort(nums);
     cout << "after sort: " << endl;
+    // printArray(nums_for_heap);
     printArray(nums);
     return 0;
 }
